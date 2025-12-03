7a4eedd58ee13bbffc53672169f5d0fbab7fc17b
Nadhifhehe <68574050+Nadhifhehe@users.noreply.github.com>
2025-12-03 17:43:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,18 @@ with kiri:
 
 with kanan:
     st.markdown("### Riwayat Transaksi")
+st.markdown("""
+<style>
+.box {
+    background-color: #ddd;
+    border-radius: 16px;
+    padding: 20px;
+    height: 300px;
+}
+</style>
+""", unsafe_allow_html=True)
+with kiri:
+    st.markdown('<div class="box">Grafik</div>', unsafe_allow_html=True)
+
+with kanan:
+    st.markdown('<div class="box">Riwayat</div>', unsafe_allow_html=True)
