3aec149a6ba1246b1a8fb9b164536acf1c9ca1a5
Nadhifhehe <68574050+Nadhifhehe@users.noreply.github.com>
2025-12-03 20:12:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,13 @@ if "page" not in st.session_state:
 if st.session_state.page == "home":
     st.title("Money Tracker")
 
+    col1, col2 = st.columns(2)
+
+with col1:
     if st.button("➡️ Ke Halaman Pemasukan"):
         st.session_state.page = "pemasukan"
 
+with col2:
     if st.button("➡️ Ke Halaman Pengeluaran"):
         st.session_state.page = "pengeluaran"
 
