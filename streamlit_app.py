b7bc8c9316580a123f9c1b9a163e01d0502d2edc
Nadhifhehe <68574050+Nadhifhehe@users.noreply.github.com>
2025-12-03 20:08:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,15 @@ import streamlit as st
 
 if "page" not in st.session_state:
     st.session_state.page = "home"
+col1, col2 = st.columns(2)
 
+with col1:
+    if st.button("➕ Pemasukan"):
+        st.session_state.page = "pemasukan"
+
+with col2:
+    if st.button("➖ Pengeluaran"):
+        st.session_state.page = "pengeluaran"
 # ---------------- HOME ----------------
 if st.session_state.page == "home":
     st.title("Money Tracker")
