9c1161d85788a09bd978d035732b53d30ebbddac
Nadhifhehe <68574050+Nadhifhehe@users.noreply.github.com>
2025-12-03 20:10:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,40 @@
 import streamlit as st
 
+import streamlit as st
+
+if "page" not in st.session_state:
+    st.session_state.page = "home"
+
 # ---------------- HOME ----------------
 if st.session_state.page == "home":
     st.title("Money Tracker")
 
-if "page" not in st.session_state:
-    st.session_state.page = "home"
-col1, col2 = st.columns(2)
-
-with col1:
-    if st.button("➕ Pemasukan"):
+    if st.button("➡️ Ke Halaman Pemasukan"):
         st.session_state.page = "pemasukan"
 
-with col2:
-    if st.button("➖ Pengeluaran"):
-        st.session_state.page = "pengeluaran"   
+    if st.button("➡️ Ke Halaman Pengeluaran"):
+        st.session_state.page = "pengeluaran"
+
+# ---------------- PEMASUKAN ----------------
+elif st.session_state.page == "pemasukan":
+    st.title("Halaman Pemasukan")
+
+    jumlah = st.number_input("Masukkan jumlah uang", 0)
+
+    if st.button("Simpan"):
+        st.success("Pemasukan disimpan!")
+
+    if st.button("⬅️ Kembali"):
+        st.session_state.page = "home"
+
+# ---------------- PENGELUARAN ----------------
+elif st.session_state.page == "pengeluaran":
+    st.title("Halaman Pengeluaran")
+
+    jumlah = st.number_input("Masukkan jumlah pengeluaran", 0)
+
+    if st.button("Simpan"):
+        st.success("Pengeluaran disimpan!")
+
+    if st.button("⬅️ Kembali"):
+        st.session_state.page = "home"
