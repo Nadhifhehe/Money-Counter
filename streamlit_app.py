8fdf0148311504b40dc28a12a1df0fdbb1d690bd
Nadhifhehe <68574050+Nadhifhehe@users.noreply.github.com>
2025-12-03 18:46:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ st.set_page_config(layout="centered")
 st.title ("Money Tracker")
 if st.button("➡️ Ke Halaman Pemasukan"):
     st.switch_page("pages/pemasukan.py")
-
+    
 if st.button("➡️ Ke Halaman Pengeluaran"):
     st.switch_page("pages/pengeluaran.py")
