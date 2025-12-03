56997c99a397d2d721bbbcf95d4de457d3c4d2b5
Nadhifhehe <68574050+Nadhifhehe@users.noreply.github.com>
2025-12-03 18:49:41 +0700

    Update pengeluaran.py

--- pages/pengeluaran.py
+++ pages/pengeluaran.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 st.title("Halaman Pengeluaran")
 
-jumlah = st.number_input("Masukkan jumlah pengeluaran:", 0)
+jumlah = st.number_input("Masukkan jumlah pengeluaran", 0)
 
 if st.button("Simpan"):
-    st.error("Pengeluaran disimpan!")
+    st.success("Pengeluaran disimpan!")
 
 if st.button("⬅️ Kembali"):
     st.switch_page("streamlit_app.py")
