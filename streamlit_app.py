5985543883c4ba07709a405774f4f94ed7db1e3c
Nadhifhehe <68574050+Nadhifhehe@users.noreply.github.com>
2025-12-03 18:48:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.title("Money Tracker")
 
-if st.button("➡️ Ke Halaman Pemasukan"):    st.switch_page("pages/pemasukan.py")
+if st.button("➡️ Ke Halaman Pemasukan"):
+    st.switch_page("pages/pemasukan.py")
 
-if st.button("➡️ Ke Halaman Pengeluaran"):    st.switch_page("pages/pengeluaran.py")
+if st.button("➡️ Ke Halaman Pengeluaran"):
+    st.switch_page("pages/pengeluaran.py")
