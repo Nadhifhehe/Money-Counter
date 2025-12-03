74c2db80b78967e4bb37e3190c81488448df74af
Nadhifhehe <68574050+Nadhifhehe@users.noreply.github.com>
2025-12-03 20:13:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,26 @@ with col2:
     if st.button("➡️ Ke Halaman Pengeluaran"):
         st.session_state.page = "pengeluaran"
 
+# ---------------- PEMASUKAN ----------------
+elif st.session_state.page == "pemasukan":
+    st.title("Halaman Pemasukan")
+
+    jumlah = st.number_input("Masukkan jumlah uang", 0)
+
+    if st.button("Simpan"):
+        st.success("Pemasukan disimpan!")
+
+    if st.button("⬅️ Kembali"):
+        st.session_state.page = "home"
+
+# ---------------- PENGELUARAN ----------------
+elif st.session_state.page == "pengeluaran":
+    st.title("Halaman Pengeluaran")
+
+    jumlah = st.number_input("Masukkan jumlah pengeluaran", 0)
+
+    if st.button("Simpan"):
+        st.success("Pengeluaran disimpan!")
+
+    if st.button("⬅️ Kembali"):
+        st.session_state.page = "home"
